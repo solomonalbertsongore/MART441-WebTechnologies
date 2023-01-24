1c74ed30697c83396c2559bd0e6dd6c0ba96f334
solly <sa144385@umconnect.umt.edu>
2023-01-24 16:58:43 -0700

    Finished the bulk of hw2

---
+++ hw2/hw2.js
@@ -0,0 +1,19 @@
+console.log("My three favorite websites are: "); 
+console.log("- twitch.tv"); 
+console.log("- youtube.com"); 
+console.log("- w3schools.com");
+console.log(""); 
+console.log("My three favorite games are: "); 
+console.log("- Valorant"); 
+console.log("- Overwatch (the original)"); 
+console.log("- Skyrim");
+console.log(""); 
+console.log("My three favorite artists are: "); 
+console.log("- Zhora Devi");
+console.log("My older sister. She is a tattoo artist and aspires to give people meaningful tattoos that will impact their life for the better."); 
+console.log("- Sophia Lark");
+console.log("My younger sister. She is 14 and just discovering all of her creative talent. She has tried many different forms of art, but is currently pursuing guitar and singing. Her willingness to learn is inspiring."); 
+console.log("- Todd Lark");
+console.log("My father. He is also a musician. His stuff is really good, and he plays just to bring joy and love to those listening.");
+console.log(""); 
+console.log("I have many more that are and aren't family, but these are the 3 I'm gonna pick for this assignment :)")
