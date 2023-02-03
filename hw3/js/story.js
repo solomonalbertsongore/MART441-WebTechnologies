91025b37229204332b0960a0af809dbfffcc8363
solly <sa144385@umconnect.umt.edu>
2023-02-02 21:43:01 -0700

    added more functionality. really just need to add story, pictures, and styling now.

--- hw3/js/story.js
+++ hw3/js/story.js
@@ -3,18 +3,31 @@
 var answeredYes = 0; 
 
 document.getElementById('scene1').style.visibility = "hidden"; 
-document.getElementById('scene2').style.visibility = "hidden"; 
+document.getElementById('scene2').style.visibility = "hidden";
+document.getElementById('scene3').style.visibility = "hidden";
+
+
 
 document.getElementById('play').onclick = function() {playGame()}; 
 
-document.getElementById('yes').onclick = function() {myFunction()};
 
+document.getElementById('yes').onclick = function() {myFunction()};
 document.getElementById('no').onclick = function() {endOfGame()};
 
 function playGame()
 {
     document.getElementById('scene1').style.visibility = "visible"; 
     document.getElementById('play').style.visibility = "hidden"; 
+}
+
+function showSceneTwo()
+{
+    document.getElementById('scene2').style.visibility = "visible"; 
+}
+
+function showSceneThree()
+{
+    document.getElementById('scene3').style.visibility = "visible"; 
 }
 
 function myFunction()
