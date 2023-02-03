ab66f65b950554487cccbaeef16eb3ece604b6bb
solly <sa144385@umconnect.umt.edu>
2023-02-03 09:41:08 -0700

    more functionality
    - gets player name
    - warning box

--- hw3/js/story.js
+++ hw3/js/story.js
@@ -1,6 +1,7 @@
 // window.alert("i am here"); 
 
 var answeredYes = 0; 
+var playerName; 
 
 document.getElementById('scene1').style.visibility = "hidden"; 
 document.getElementById('scene2').style.visibility = "hidden";
@@ -11,6 +12,7 @@ document.getElementById('scene3').style.visibility = "hidden";
 document.getElementById('play').onclick = function() {playGame()}; 
 
 
+
 document.getElementById('yes').onclick = function() {myFunction()};
 document.getElementById('no').onclick = function() {endOfGame()};
 
@@ -18,6 +20,18 @@ function playGame()
 {
     document.getElementById('scene1').style.visibility = "visible"; 
     document.getElementById('play').style.visibility = "hidden"; 
+
+    // Get's the players name and shows it on the page
+    let playerName = prompt("What is your name?"); 
+    // https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_prompt
+    if(playerName != "" && playerName != null)
+    {
+        document.getElementById('hiPlayer').innerHTML = "Hello " + playerName; 
+    }
+    else
+    {
+        document.getElementById('hiPlayer').innerHTML = "You should have entered a name :("; 
+    }
 }
 
 function showSceneTwo()
