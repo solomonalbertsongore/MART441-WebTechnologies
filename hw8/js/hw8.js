b46cc1edf18aaef35fa9b8af2fe7f653f9a75271
solly <sa144385@umconnect.umt.edu>
2023-03-27 09:10:33 -0600

    started to add shapes

--- hw8/js/hw8.js
+++ hw8/js/hw8.js
@@ -4,16 +4,14 @@
 './imgs/bird.png', './imgs/charizard.png', './imgs/diamond.png', './imgs/egg.png', './imgs/evee.png']; 
 */
 
-let images = [new fadedImg('./imgs/bird.png', "This is a bird"),
-new fadedImg('./imgs/charizard.png', "This is a dragon"),
-new fadedImg('./imgs/diamond.png', "This is a weird lizard"),
+let images = [new fadedImg('./imgs/bird.png', "This is a bird", 'square'),
+new fadedImg('./imgs/charizard.png', "This is a dragon", 'circle'),
+new fadedImg('./imgs/diamond.png', "This is a weird lizard", 'rectangle'),
 new fadedImg('./imgs/egg.png', "This is an egg"),
 new fadedImg('./imgs/evee.png', "This is a cute fox/dog"),
 ]; 
 
 
-const canvas = document.querySelector('canvas'); 
-const context = canvas.getContext('2d'); 
 
 for(let i = 0; i < images.length; i++)
 {
@@ -39,8 +37,28 @@ function loopStuff()
         $('#words').fadeIn(2000).fadeOut(2000, function(){
         $('#words').text(images[i].words)})
         .fadeIn(2000);
+        
+        // setInterval(changeTxtBig(), 500); 
+        setInterval(changeTxtSmall, 500);
+
+        $('.shape').fadeIn(2000).fadeOut(2000, function(){
+        $('.shape').attr('class', images[i].shape)})
+        .fadeIn(2000);
+
+        // $('.shape').attr('class', images[i].shape); 
+        
         })
-})
+    })
+}
+
+function changeTxtBig()
+{
+    $('#words').css('fontSize', '55px')
+}
+
+function changeTxtSmall()
+{
+    $('#words').css('fontSize', '22px')
 }
 
 
