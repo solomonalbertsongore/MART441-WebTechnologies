b7881f7c55574d17e639e086baf8eccd9a681884
solly <sa144385@umconnect.umt.edu>
2023-04-03 22:38:35 -0600

    Update hw9.js

--- hw9/js/hw9.js
+++ hw9/js/hw9.js
@@ -1,12 +1,24 @@
+// retrieve/display data
 $(document).ready(function(){
   $("button").click(function(){
     $.getJSON("./json/pokedex.json", function(result){
       $.each(result, function(i, field){
         $("div").append(JSON.stringify(field));
       });
-    });
+    })
+
+    $('div').stylish();
   });
 });
+
+// plugin 
+$.fn.stylish = function() 
+{
+    this.css('background-color', 'black'); 
+    this.css('color', 'white'); 
+    this.css('font-size', 12); 
+}
+
 
 /*$(document).ready(function(){
   $("button").click(function(){
