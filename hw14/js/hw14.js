3dafc0fa20c73c8318e8b08ce57926a77d8ae3f6
solly <sa144385@umconnect.umt.edu>
2023-04-27 21:33:44 -0600

    tried to get my own models to show, no success

--- hw14/js/hw14.js
+++ hw14/js/hw14.js
@@ -1,3 +1,5 @@
+// import { FBXLoader } from 'FBXLoader.js';
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -5,14 +7,18 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+// 1st object - cube
 var box = new THREE.BoxGeometry();
 var material = new THREE.MeshBasicMaterial({
     color: 0x00ff00
 });
 var cube = new THREE.Mesh(box, material);
 scene.add(cube);
+cube.position.x = -4; 
+cube.position.y = 0; 
+cube.position.z = 0;
 
-// from three.js docs
+// 2nd obejct, lines -- from three.js docs
 const geometry = new THREE.SphereGeometry( 100, 100, 100 );
 
 const wireframe = new THREE.WireframeGeometry( geometry );
@@ -24,14 +30,43 @@ line.material.transparent = true;
 
 scene.add( line );
 
+// 3rd object, modeled sword
+
+/*
+const loader = new FBXLoader();
+
+loader.load( '../fbx/swordTextured.fbx', function ( fbx ) {
+
+	scene.add( fbx.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+*/
+
+function loadModel() {
+    var loader = new THREE.OBJLoader();
+    loader.load(
+      "../obj/alien.obj",
+      function (object) {
+        object.rotation.z = Math.PI;
+        scene.add(object);
+        document.querySelector("h1").style.display = "none";
+      }
+    );
+  }
+
 camera.position.z = 5;
 
 function animate() {
     requestAnimationFrame( animate );
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-    line.rotation.x += 0.01;
-    line.rotation.y += 0.01;
+    line.rotation.x += 0.001;
+    line.rotation.y += 0.001;
     renderer.render( scene, camera );
 }
 animate();
+loadModel(); 
