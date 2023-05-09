56f6ba29f7fb117558c3b6530132919de03ce37a
solly <sa144385@umconnect.umt.edu>
2023-05-09 12:31:40 -0600

    fixed hamburger menu

--- final/js/nav.js
+++ final/js/nav.js
@@ -1,8 +1,18 @@
+var closeIcon = document.getElementById("closeIcon"); 
+var menuIcon = document.getElementById("menuIcon"); 
+
+
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
       x.style.display = "none";
+      closeIcon.style.display = "none"; 
+      menuIcon.style.display = "block"; 
     } else {
       x.style.display = "block";
+      closeIcon.style.display = "block"; 
+      menuIcon.style.display = "none"; 
     }
   }
+
+closeIcon.style.display = "none"; 
