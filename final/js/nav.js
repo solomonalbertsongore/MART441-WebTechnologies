22d7608834bcf9a78ea689bbdf1cc1e500918f26
solly <sa144385@umconnect.umt.edu>
2023-05-10 14:03:05 -0600

    got the jquery hamburger working

--- final/js/nav.js
+++ final/js/nav.js
@@ -2,14 +2,23 @@ var closeIcon = document.getElementById("closeIcon");
 var menuIcon = document.getElementById("menuIcon"); 
 var x = document.getElementById("myLinks");
 
+
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
-      x.style.display = "none";
+      // jQuery for fancy hamburger navigation
+      $("#myLinks").fadeOut(function ()
+      {
+        console.log('animation complete'); 
+        x.style.display = "none"; 
+        // $("#myLinks").style("display", "none"); 
+      });
+      // x.style.display = "none"; 
       closeIcon.style.display = "none"; 
       menuIcon.style.display = "block"; 
     } else {
-      x.style.display = "block";
+      // x.style.display = "block";
+      $("#myLinks").fadeIn(); 
       closeIcon.style.display = "block"; 
       menuIcon.style.display = "none"; 
     }
@@ -17,7 +26,17 @@ function myFunction() {
 
 
 closeIcon.style.display = "none"; 
+x.style.display = "none"; 
 
+/*
+$(document).ready(function ()
+{
+  $(".hamburger").click(function()
+  {
+    $("#myLinks").fadeIn(); 
+  })
+})
+*/
 /*
 $("#hamburger").click(function(){
   $("#myLinks").fadeIn();
